a9d95eea0a0068fd50963f1ef85e60c509b14554
Igor Souza <igorrs.rib@gmail.com>
2022-08-07 17:14:29 -0300

    função listar assentos criada, arquivos adicionados

--- Prolog/Funcionario.pl
+++ Prolog/Funcionario.pl
@@ -21,8 +21,9 @@ menuFunc(Menu):-
 
 
 escolhaDeOpcao(0,Menu):- listaDescontos(), menuFunc(Menu).
+escolhaDeOpcao(1,Menu):- listaAssentos(), menuFunc(Menu).
+
 /
-escolhaDeOpcao(3,Menu):- excluirFuncionario(Menu), menuEmpresa(Menu).
 escolhaDeOpcao(4,Menu):- listaTodosFuncionarios(), menuEmpresa(Menu).
 escolhaDeOpcao(6,Menu):- listaValoresDeCadaTipo(), menuEmpresa(Menu).
 escolhaDeOpcao(7,Menu):- cadastraDesconto(Menu), menuEmpresa(Menu).
@@ -33,3 +34,11 @@ escolhaDeOpcao(_, Menu):- writeln('ok').
 listaDescontos():- writeln("\n       -----TODOS OS DESCONTOS DISPONIVEIS NO SISTEMA!-----\n"),
       lerArquivoCsv('descontos.csv',Resultado),
       writeln(Resultado).
+
+listaAssentos():- writeln("\n-----TODOS ASSENTOS ECONOMICOS DISPONIVEIS----\n"),
+lerArquivoCsv('assentos_economico_disponiveis.csv',Resultado),
+writeln(Resultado),
+
+writeln("\n-----TODOS ASSENTOS EXECUTIVOS DISPONIVEIS----\n"),
+lerArquivoCsv('assentos_executivo_disponivel.csv',Exec),
+writeln(Exec).
